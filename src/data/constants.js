35e14c279dbf79d9c3ab6b92623ca3a11bd8800a
Ayush Paul <paulayus323@gmail.com>
2023-12-26 12:40:51 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,261 @@
+export const Bio = {
+  name: "Ayush Paul",
+  roles: [
+    "Web Developer",
+    "Video Editor",
+    "Programmer",
+    "Audio Editor",
+    "Graphics Designer",
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/AyushPaul323",
+  resume:
+    "https://drive.google.com/file/d/1iGkY2ZbhgaTxvNnQdSdLmpaoypg1f8vS/view?usp=drive_link",
+  linkedin:
+    "https://www.linkedin.com/in/ayush-paul-50473b214?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  insta: "https://www.instagram.com/ayush_paul_13?igsh=ZjdzcnZsbDFqNnU4",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+      {
+        name: "DBMS",
+        image:
+          "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703404192/unnamed_hobbls.png",
+      },
+    ],
+  },
+  {
+    title: "Extra",
+    skills: [
+      {
+        name: "Filmora12",
+        image:
+          "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703404751/Wondershare-12-tools_wgtwgh.webp",
+      },
+      {
+        name: "Fl Studio",
+        image:
+          "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703404853/images_1_nba6kv.jpg",
+      },
+      {
+        name: "Audiocity",
+        image:
+          "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703404889/images_2_imqevm.jpg",
+      },
+      {
+        name: "Movie Maker",
+        image:
+          "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703404933/download_z7jlx3.jpg",
+      },
+      {
+        name: "C",
+        image:
+          "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703404064/c-tutorial-for-beginners_l9usoz.png",
+      },
+      {
+        name: "Java",
+        image:
+          "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703404141/Java-Logo_tbzfxj.png",
+      },
+      {
+        name: "DBMS",
+        image:
+          "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703404192/unnamed_hobbls.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703404229/640px-Python.svg_kh3ijv.png",
+      },
+      {
+        name: "Image Processing",
+        image:
+          "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703404277/seven-innovative-projects-on-image-processing-min_zz17m2.jpg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Canva",
+        image:
+          "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703404568/Canva-logo_dqfijq.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img:"https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703405208/logo.e13bd9e7c82e5e8bb6fe_o0djry.png",
+    role: "Smart Bengal Hackathon Finalist 2023",
+    desc: "A Smart Bengal Hackathon is likely an event where innovators, programmers, designers, and other tech enthusiasts come together to collaboratively work on solutions to address challenges and improve various aspects of Bengal (assuming Bengal refers to a region or city). The focus of such a hackathon is likely on leveraging technology, data, and creative ideas to make Bengal smarter, more efficient, and more sustainable.",
+  },
+  {
+    id: 0,
+    img: "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703405429/download_1_fdcj69.jpg",
+    role: "Finalist of Market Mayhem 2023",
+    desc: "A event had been organised by scottish church college to brand our product and sell them.",
+  },
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703406106/images_cus30t.png",
+    role: "Finalist of CIEM Hacakthon 2023",
+    desc: "The CIEM Internal Hackathon 2023 is an exciting 8-hour event where employees or students within CIEM come together to showcase their creativity, problem-solving skills, and technical expertise. Participants will work collaboratively in teams or as individuals to develop innovative solutions, prototypes, or projects within a limited timeframe.",
+  },
+  {
+    id: 2,
+    role: "Volunteered at YPMC 2022",
+    desc: "Organised by Scottish Church College",
+  },
+  {
+    id: 3,
+    img: "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703406530/International-Mathematical-Olympiad_seztlx.jpg",
+    role: "Bronze Medalist of IMO 2019",
+    desc: "The International Mathematical Olympiad is an annual mathematics competition for high school students from around the world. The participants, who are some of the brightest young mathematicians, compete by solving challenging mathematical problems. The problems are designed to test their problem-solving skills, mathematical creativity, and deep understanding of various mathematical concepts.",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703407130/Scottish_Church_College_logo_infkxz.png",
+    school: "Scottish Church College",
+    date: "Oct 2021 - Sep 2024",
+    grade: "7.6 CGPA",
+    desc: "I am currently pursuing a Bsc in Computer Science at Scottish Church College. I have completed 4 semesters and have a CGPA of 7.6. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Bsc in Computer Science",
+  },
+  {
+    id: 1,
+    img: "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703407342/schoollogo_kxv5j6.png",
+    school: "St Stephen's School",
+    date: "March 2018 - March 2019",
+    grade: "89%",
+    desc: "I completed my class 10 at , St Stephen's School , where I studied Science with Computer Science.",
+    degree: "ICSC(X), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703407342/schoollogo_kxv5j6.png",
+    school: "St Stephen's School",
+    date: "March 2020 - Apr 2021",
+    grade: "94%",
+    desc: "I completed my class 12 education at high school atSt Stephen's School, where I studied Science with Computer Application.",
+    degree: "ISC(XII), Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Health Center",
+    description: "Introducing our innovative Health Center Project, where your well-being is at the forefront of our digital care. Our user-friendly website provides a seamless experience, allowing you to effortlessly check the availability of beds,securely book doctor appointments, and conveniently store essential health documents in one secure location.Discover the ease of navigating through real-time bed availability, ensuring you have the information you needwhen it matters most. Our intuitive platform empowers you to schedule doctor appointments with just a fewclicks, guaranteeing a hassle-free and secure booking process.Your health records are kept safe and accessible within our robust storage system. Effortlessly upload andorganize crucial documents such as medical records and prescriptions, giving you peace of mind and quickaccess to your health history whenever you need it.In times of urgency, our website goes beyond expectations by offering emergency services coupled with anefficient SMS system. Receive timely alerts and notifications, providing you with the support and information yourequire during critical moments.Additionally, our website acts as a hub for addressing various health-related concerns. Explore informativearticles, engage in discussions, and find solutions to common health queries, fostering a community dedicatedto well-being.Experience a new era of healthcare accessibility with our Health Center Project – where technology meetscompassion, and your health is our priority. Welcome to a digital space designed to enhance and simplify yourhealthcare journey.",
+    image:"https://res.cloudinary.com/dxxjc0u2h/image/upload/v1679332664/Screenshot__704_-removebg-preview_jdspqe.png",
+    videoUrl:"https://res.cloudinary.com/dxxjc0u2h/video/upload/v1703414467/Ayush_aeyrtl.mp4",
+    tags: [
+      "Firebase",
+      "Email Js",
+      "React Js",
+      "Node Js",
+      "Express Js",
+    ],
+    category: "web app",
+  },
+  {
+    id: 0,
+    title: "Minitry Of Ayush",
+    description:
+      "Welcome to Venture India, an empowering initiative under the Ministry of Ayush, where connections flourish,collaborations thrive, and dreams take flight. Our comprehensive website opens doors to a vibrant ecosystem,uniting investors, stockholders, incubators, accelerators, and government entities in a revolutionary platform.At the heart of Venture India is a cutting-edge chatting web app, your gateway to seamless communication.Connect effortlessly with investors, incubators, and fellow enthusiasts. Exchange ideas, forge partnerships, andpropel your ventures forward through real-time collaboration.Dive into our Instagram-inspired community, a virtual haven where achievements are celebrated and experiencesare shared. Post your milestones, share insights, and engage with a community driven by passion andinnovation. It's more than a platform; it's a space where your success stories resonate, inspiring others to pursuetheir dreams.But that's not all. Venture India takes mentorship to new heights with virtual mentoring opportunities. Seekguidance from seasoned professionals, learn from their journeys, and navigate the complexities of yourentrepreneurial path with confidence.Fuel your motivation further with the Achiever's Section, a source of inspiration where users showcase theirtriumphs. Immerse yourself in a tapestry of success narratives, each story a testament to perseverance and thelimitless potential within the Venture India community.Join us on this transformative journey where connections are made, achievements are celebrated, andaspirations are realized. Venture India: Bridging aspirations, forging connections, and shaping the future ofentrepreneurship.",
+    image:
+      "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1695578577/1-removebg-preview_wzktsf.png",
+    tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    category: "web app",
+    videoUrl:"https://res.cloudinary.com/dxxjc0u2h/video/upload/v1703416680/PLA_3N_SIH2K23_nisxp5.mp4",
+  },
+  {
+    id: 1,
+    title: "SocioSpark",
+    description:
+      "Embark on a new era of community collaboration with Socio Spark, your go-to platform for seamless event sponsorship. Picture a world where users effortlessly discover sponsorships for their events or become sponsors themselves, all within the confines of a dynamic and user-friendly app. Socio Spark isn't just an app; it's a visionary project that aims to connect event organizers with potential sponsors, fostering partnerships that elevate every occasion. Imagine finding the perfect match for your event's vision or seamlessly contributing to a cause that resonates with you. Our prototype is just the beginning — the future holds a digital space where individuals and businesses converge, sharing their passions and support for a multitude of events. From cultural celebrations to philanthropic endeavors, Socio Spark is the catalyst for turning visions into reality. Stay tuned as we transform the landscape of event sponsorship, making collaboration as simple as a tap on your screen. Socio Spark: Igniting partnerships, sparking innovation, and making dreams come alive in the world of events.",
+    image:
+      "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703416941/ayush_hoewvr.png",
+    tags: [
+      "React Native",
+      "MongoDb",
+      "Node Js",
+      "Flutter",
+    ],
+    category: "android app",
+  },
+  {
+    id: 2,
+    title: "Avalanches Detection",
+    description:
+      "Detection Welcome to our groundbreaking project designed to enhance avalanche safety in accident-prone areas. Combining state-of-the-art hardware and software solutions, we've created a comprehensive system to identify potential avalanche incidents and trigger timely rescue responses. In our hardware model, we leverage three crucial factors for accurate predictions: magnitude calculation, impact assessment upon hitting the ground, and consideration of temperature and barometric conditions. This multi- faceted approach enhances our ability to assess and anticipate avalanche risks effectively. Our intelligent system incorporates a threshold factor, allowing users to set specific parameters. When these thresholds are crossed, a signal is generated, triggering an automatic alert. This alert is seamlessly transmitted to rescuers through our website and app, ensuring swift and targeted response to potential incidents. Powered by React.js and map APIs, our software interface provides a dynamic alert system. In the event of an avalanche risk, the location on the map blinks, providing rescuers with real-time, visual information to assess and respond promptly. This intuitive approach enhances situational awareness, enabling rescue teams to understand and address the situation efficiently. Join us in revolutionizing avalanche safety, where cutting-edge technology meets proactive rescue measures. Together, we are creating a safer environment for those in accident-prone regions, ensuring that help is just a click away.",
+    image:
+      "https://res.cloudinary.com/dxxjc0u2h/image/upload/v1703417245/Screenshot_99_afcrul.png",
+    tags: ["C++", "React" , "Flutter"],
+    category: "Hardware Software",
+  },
+];
